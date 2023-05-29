d34adac981e9a5db9519a125c09eafccbc9debbc
Jan Hadl <jan.d.hadl@gmail.com>
2023-05-29 19:13:04 +0200

    added finetuning for clip

--- src/model/clip_model.py
+++ src/model/clip_model.py
@@ -5,7 +5,7 @@ class CLIPModel(BaseModel):
     def __init__(self, gpu):
         super().__init__(img_size=224, gpu=gpu)
 
-        self.model = TCLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(gpu)
+        self.model = TCLIPModel.from_pretrained("./model_snapshots/model_20230528_195959_1_0.47").to(gpu)
         self.image_processor = CLIPImageProcessor.from_pretrained("openai/clip-vit-base-patch32")
         self.tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
 
